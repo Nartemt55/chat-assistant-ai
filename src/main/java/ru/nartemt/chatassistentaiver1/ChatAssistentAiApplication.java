23736e32f5e1357bc5673da188df14284cba21b5
Nartemt55 <nartemt@gmail.com>
2026-05-01 14:21:29 +0300

    initial commit, enviroment is prepared

---
+++ src/main/java/ru/nartemt/chatassistentaiver1/ChatAssistentAiApplication.java
@@ -0,0 +1,13 @@
+package ru.nartemt.chatassistentaiver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAssistentAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAssistentAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nartemt/chatassistentaiver1/ChatAssistentAiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nartemt.chatassistentaiver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAssistentAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
